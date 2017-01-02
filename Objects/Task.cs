0314bad6c798df5f3176d574bccfc1e4926d4093
Brent Perry <burentoperii@gmail.com>
2017-01-02 10:10:32 -0600

    add/display/clear tasks.

---
+++ Objects/Task.cs
@@ -0,0 +1,38 @@
+using System.Collections.Generic;
+
+namespace ToDoList.Objects
+{
+  public class Task
+  {
+    private string _description;
+    private static List<string> _instances = new List<string>{};
+
+    public Task (string description)
+    {
+      _description = description;
+    }
+    public string GetDescription()
+    {
+      return _description;
+    }
+    public void SetDescription(string newDescription)
+    {
+      _description = newDescription;
+    }
+
+    public static List<string> GetAll()
+    {
+      return _instances;
+    }
+
+    public static void ClearAll()
+    {
+      _instances.Clear();
+    }
+
+    public void Save()
+    {
+      _instances.Add(_description);
+    }
+  }
+}
